6e44ce7525688e0f32da2ebe762bd10534f62c69
Christoph Sauer <sauer@nuveon.de>
2021-01-17 10:52:05 +0100

    update server from 1.15 to 1.16

--- setup/server-run.sh
+++ setup/server-run.sh
@@ -3,6 +3,6 @@
 java -jar \
 -Xms2G -Xmx2G \
 -Dcom.mojang.eula.agree=true \
-/opt/minecraft/paper-229.jar nogui \
+/opt/minecraft/paper-1.16.5-428.jar nogui \
 --plugins /opt/minecraft/plugins \
 --port 25565
