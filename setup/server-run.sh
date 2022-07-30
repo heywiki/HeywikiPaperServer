2d1348be4fc279284a6e9c8a7d7a006c2f47472c
nuveon-enterprise <info@nuveon.de>
2022-07-30 15:30:07 +0200

    Updated to version 1.19

--- setup/server-run.sh
+++ setup/server-run.sh
@@ -3,6 +3,6 @@
 java -jar \
 -Xms2G -Xmx2G \
 -Dcom.mojang.eula.agree=true \
-/opt/minecraft/paper-1.16.5-428.jar nogui \
+/opt/minecraft/paper-1.19.1-91.jar nogui \
 --plugins /opt/minecraft/plugins \
 --port 25565
